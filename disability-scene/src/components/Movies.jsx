d2c8e79a6b27804f15eeb51d57c21eef9fe93cee
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-10 16:44:48 -0400

    added movies component

--- disability-scene/src/components/Movies.jsx
+++ disability-scene/src/components/Movies.jsx
@@ -1,5 +1,47 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 const Movies = () => {
-    return <h2>This is Movies</h2>
+    const [movies, setMovies] = useState([])
+
+
+    useEffect(() => {
+        const getMovies = async () => {
+            try {
+                const response = await axios.get(`https://disability-scene-api-production.up.railway.app/movies`);
+                const data = response.data;
+                setMovies(data);
+            } catch (error) {
+                console.error("Error fetching movies:", error);
+            }
+        }
+        getMovies();
+    }, [movies])
+
+    let navigate = useNavigate()
+
+
+    const showMovie = (id) => {
+        navigate(`moviePage/${id}`)
+    }
+    
+    return ( 
+        <div className="movies-grid">
+            <h2>MOVIES</h2>  
+            <div className="grid">
+                {
+                   movies.map((movie) => (
+                    <div key={movie.id} className="movieList" onClick={() => showMovie(movie.id)}>
+                        <img className="individualImage" aria-hidden="true" width="203px" height="258px" src={movie.img}></img>
+                        <p className="individualTitle">{movie.title}</p>
+                    </div>
+                   ))
+                }
+            </div>
+
+        </div>
+    )
 }
 
 export default Movies
