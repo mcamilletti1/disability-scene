cc536f727c72013862063015579dde424c71ff29
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-10 16:16:49 -0400

    updated Main

--- disability-scene/src/components/Main.jsx
+++ disability-scene/src/components/Main.jsx
@@ -4,13 +4,14 @@ import Movies from './Movies'
 import TVShows from './TVShows'
 import LeaveAReview from './LeaveAReview'
 import MoviePage from './MoviePage'
+import PropTypes from 'prop-types'
 //import ActorPage from './ActorPage'
 
-const Main = () => {
+const Main = ({ showSearch }) => {
     return (
         <div className ="routes-container">
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home showSearch={showSearch} />} />
                 <Route path="/movies" element={<Movies />} />
                 <Route path="/TVshows" element={<TVShows />} />
                 <Route path="/leaveAReview/:id" element={<LeaveAReview />} />
@@ -19,5 +20,10 @@ const Main = () => {
         </div>
     )
 }
+
+Main.propTypes = {
+    showSearch: PropTypes.bool.isRequired
+}
+
 
 export default Main
