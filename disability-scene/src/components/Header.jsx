7dbd1a6c30dae048d4961eb7ed0551df5c27ebe2
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-10 16:06:37 -0400

    updated search functionality

--- disability-scene/src/components/Header.jsx
+++ disability-scene/src/components/Header.jsx
@@ -4,11 +4,16 @@ import PropTypes from 'prop-types'
 
 
 const Header = ({ onSearchButtonClick }) => {
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        onSearchButtonClick()
+    }
+
     return (
         <div className="header">
             <Nav/>
-            <form id="searchForm" role="search">
-                <button id="searchButton" type="button" width="16px" height="16px" aria-label="Search button" onClick={onSearchButtonClick}><img width="16px" height="16px" aria-hidden="true" src="src/assets/search-sharp.svg"/></button>
+            <form id="searchForm" role="search" onSubmit={handleFormSubmit}>
+                <button id="searchButton" type="submit" width="16px" height="16px" aria-label="Search button"><img width="16px" height="16px" aria-hidden="true" src="src/assets/search-sharp.svg"/></button>
                 <input id="searchBar" type="text" name="searchInput" placeholder="Search..." aria-label="Search bar"/>
             </form>
         </div>
