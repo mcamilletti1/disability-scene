00dd13b1a56f715f662d5f558493578155ddb717
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-10 11:18:57 -0400

    set up movie page component

--- disability-scene/src/components/Main.jsx
+++ disability-scene/src/components/Main.jsx
@@ -3,8 +3,7 @@ import Home from './Home'
 import Movies from './Movies'
 import TVShows from './TVShows'
 import LeaveAReview from './LeaveAReview'
-//import MoviePage from './MoviePage'
-//import LeaveAReview from './LeaveAReview'
+import MoviePage from './MoviePage'
 //import ActorPage from './ActorPage'
 
 const Main = () => {
@@ -15,6 +14,7 @@ const Main = () => {
                 <Route path="/movies" element={<Movies />} />
                 <Route path="/TVshows" element={<TVShows />} />
                 <Route path="/leaveAReview/:id" element={<LeaveAReview />} />
+                <Route path="/moviePage/:id" element={<MoviePage />} />
             </Routes>
         </div>
     )
--- disability-scene/src/components/MoviePage.jsx
+++ disability-scene/src/components/MoviePage.jsx
@@ -1,9 +1,31 @@
 import Actors from './Actors'
 import MovieInfo from './MovieInfo'
 import Reviews from './Reviews'
+import { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
 
 
 const MoviePage = () => {
+    const [movie, setMovie] = useState('')
+    let { id } = useParams()
+    
+    useEffect(() => {
+        const getMovie = async () => {
+            try {
+                const response = await axios.get(`https://disability-scene-api-production.up.railway.app/movies`);
+                const data = response.data;
+                const moviesLength = data.length;
+                const response2 = await axios.get(`https://disability-scene-api-production.up.railway.app/movies/${moviesLength}`);
+                const featuredMovie = response2.data;
+                setMovie(featuredMovie);
+            } catch (error) {
+                console.error("Error fetching movie:", error);
+            }
+        }
+        getMovie();
+    }, [movie, id])
+
     return (
         <div className="movie-page">
             <MovieInfo />
