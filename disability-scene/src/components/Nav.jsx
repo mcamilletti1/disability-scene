51d5ab0d732e6fff489ed7a163667caac2ab8e07
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-16 15:08:01 -0400

    updated images

--- disability-scene/src/components/Nav.jsx
+++ disability-scene/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
     return (
         <div className="nav-links">
-            <Link class="nav" to="/"><img id="logo" src="src/assets/logo_on_white.svg" alt="Disability Scene logo" width="125px" height="20px"></img></Link>
+            <Link class="nav" to="/"><img id="logo" src="public/assets/logo_on_white.svg" alt="Disability Scene logo" width="125px" height="20px"></img></Link>
             <Link class="nav" to="/movies"><button id="moviesButton"><strong>MOVIES</strong></button></Link>
             <Link class="nav" to="/TVshows"><button id="tvButton"><strong>TV SHOWS</strong></button></Link>
         </div>
