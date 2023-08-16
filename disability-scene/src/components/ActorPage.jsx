15400065ee9c10de5e3bd73000d6c4ae4166e948
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-16 14:04:13 -0400

    updated actor page

--- disability-scene/src/components/ActorPage.jsx
+++ disability-scene/src/components/ActorPage.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
+
 const ActorPage = () => {
-    return <h2>This is the Actor Page</h2>
+    const [actor, setActor] = useState('')
+    let { id } = useParams()
+
+    useEffect(() => {
+        const getActor = async () => {
+            try {
+                const response = await axios.get(`https://disability-scene-api-production.up.railway.app/cast/${id}`);
+                const featuredActor = response.data;
+                setActor(featuredActor);
+            } catch (error) {
+                console.error("Error fetching actor:", error);
+            }
+        }
+        getActor();
+    }, [])
+
+
+    return (
+        <div className="actor-page">
+            <section className="actor-photo">
+                <h2>{actor.name}</h2>
+                <img width="203px" height="300px" src={actor.img}></img>
+            </section>
+            <section className="actor-details">
+                <p>Credits: {actor.credits}</p>
+                <p>Known for: {actor.title}</p>
+            </section>
+        </div>
+    )
 }
 
 export default ActorPage
