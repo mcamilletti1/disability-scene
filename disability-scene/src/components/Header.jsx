48ca257fbf5a196e5771ed9636a1c827a4494ade
Margaret Camilletti <mhcamilletti@gmail.com>
2023-12-05 15:07:13 -0500

    header component

--- disability-scene/src/components/Header.jsx
+++ disability-scene/src/components/Header.jsx
@@ -1,30 +1,57 @@
-import { useState } from 'react'
-import Nav from './Nav'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import Nav from './Nav';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = ({ onSearchSubmit }) => {
   const [searchText, setSearchText] = useState('');
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    onSearchSubmit(searchText)
+  // Function to handle input changes
+  const handleInputChange = (e) => {
+    const newText = e.target.value;
+    setSearchText(newText);
+
+    // Call the search function with the updated text
+    onSearchSubmit(newText);
   };
 
   return (
     <div className="header">
-    <Nav/>
-    <form id="searchForm" role="search" onSubmit={handleFormSubmit}>
-        <label htmlFor="searchBar" className="visually-hidden">Search</label>
-        <input id="searchBar" type="text" name="searchInput" placeholder="Search..." aria-label="Search bar" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
-        <Link to="/searchResults"><button id="searchButton" type="submit" width="16px" height="16px" aria-label="Search button"><img width="16px" height="16px" aria-hidden="true" onClick={handleFormSubmit} src="/assets/search-sharp.svg"/></button></Link>
-    </form>
+      <Nav />
+      <form id="searchForm" role="search">
+        <label htmlFor="searchBar" className="visually-hidden">
+          Search
+        </label>
+        <input
+          id="searchBar"
+          type="text"
+          name="searchInput"
+          placeholder="Search..."
+          aria-label="Search bar"
+          value={searchText}
+          onChange={handleInputChange} // Update search results as the user types
+        />
+        <Link to="/searchResults">
+          <button
+            id="searchButton"
+            type="button" // Change to "button" to prevent form submission
+            aria-label="Search button"
+          >
+            <img
+              width="16px"
+              height="16px"
+              aria-hidden="true"
+              src="/assets/search-sharp.svg"
+            />
+          </button>
+        </Link>
+      </form>
     </div>
-  )
+  );
 };
 
 Header.propTypes = {
-    onSearchSubmit: PropTypes.func.isRequired
-}
+  onSearchSubmit: PropTypes.func.isRequired,
+};
 
 export default Header;
